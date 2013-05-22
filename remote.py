ad93347f438dd22b64b36995475b6c1e2913d42e
Anton Osten <anton@ostensible.me>
2013-05-22 16:44:59 -0500

    initial commit

---
+++ remote.py
@@ -0,0 +1,51 @@
+import serial
+import multiprocessing as mp
+
+class CustomError(Exception):
+    def __init__(self, message):
+        self.message = message
+        
+    def __str__(self):
+        return self.message
+
+class BadInputError(CustomError):
+    pass
+
+class BTConnectionError(CustomError):
+    pass
+
+class Remote(object):
+    def __init__(self, port, baudrate=115200):
+        try:
+            self.ser = serial.Serial(port, baudrate)
+        except (FileNotFoundError, OSError, serial.serialutil.SerialException) as e:
+            raise BTConnectionError(e)
+    
+    def send(self, data):
+        '''Accepts either bytes or str'''
+        # check if its bytes or str
+        if not isinstance(data, (bytes, str)):
+            raise BadInputError('Expected bytes or str')
+        # if it's str, convert it to bytes
+        if type(data) is str:
+            data = bytes(data, 'ascii')
+        self.ser.write(data)
+        return len(data)
+    
+    def receive(self, timeout=None):
+        '''Reads information sent over.
+        If a timeout is specified, the reading process is killed after the timeout.'''
+        # start another process to kill in case it's taking too long
+        with mp.Pool(processes=1) as p:
+            r = p.apply_async(self.ser.read)
+            try:
+                received_data = r.get(timeout)
+            except mp.TimeoutError:
+                return 'timeout'
+        try:
+            output = received_data.decode('utf-8')
+        except UnicodeDecodeError:
+            output = received_data
+        
+        return output
+        
---
+++ run_remote.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.3
+
+from remote import Remote, BTConnectionError
+
+def main():
+    print('Initialising...')
+    
+    port = '/dev/tty.RN42-B597-SPP'
+    
+    connected = False
+    
+    while not connected:
+        try:
+            rem = Remote(port)
+        except BTConnectionError:
+            print('\x1b[31;1mConnection on port {} unsuccessful\x1b[0m'.format(port))
+            port = input('Please enter the right serial port: ')
+            print('attempting another connection...')
+        else:
+            connected = True
+    
+    while True:
+        data = input('Input: ')
+        rem.send(data)
+        rem.send('0')
+
+if __name__ == '__main__':
+    main()
